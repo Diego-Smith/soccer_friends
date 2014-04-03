c4c7ced6283f1243d8434e46b79128387ecb6155
diego_smith <diego.mailaddress@gmail.com>
2014-04-03 18:54:36 +0200

    removed cache

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,8 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+//    Ok(views.html.index("Your new application is ready."))
+    val status = Results.Status(200)
+    status(views.html.index("Your new application is ready."))
   }
-
 }
