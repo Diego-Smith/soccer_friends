e10844fd77401c6455e175e6df3610844bb4d7e6
Diego-Smith <diego.naali@gmail.com>
2014-05-05 15:05:44 +0200

    Clean friendship test

--- test/services/FriendshipSpec.scala
+++ test/services/FriendshipSpec.scala
@@ -19,22 +19,6 @@ import it.sf.models.{Friendship, User}
 @RunWith(classOf[JUnitRunner])
 class FriendshipSpec extends Specification {
 
-  val appWithMemoryDatabase = FakeApplication(additionalConfiguration = inMemoryDatabase())
-
-  //  abstract class WithDbData extends WithApplication() {
-  //    override def around[T: AsResult](t: => T): Result = super.around {
-  //      setupData()
-  //      t
-  //    }
-  //
-  //    def setupData() {
-  //      println("test")
-  //      StartupTableFiller.startupFill
-  //
-  //    }
-  //  }
-
-
   "Friendship Service" should {
 
     "insert correctly the friendship record" in new WithApplication() {
@@ -50,13 +34,6 @@ class FriendshipSpec extends Specification {
       user1 must not be empty
       user2 must not be empty
       user3 must empty
-      println(s"${user3Result.errorMessage}")
-
-//      val us1 = userService.findUserByUsername("u1")
-//      val us2 = userService.findUserByUsername("u2")
-
-//      us1 must not be empty
-//      us2 must not be empty
 
       private val friendship = friendshipService.insertFriendship(Friendship(None, user1.get.id.get, user2.get.id.get)).friendship
       private val friendship1 = friendshipService.insertFriendship(Friendship(None, user2.get.id.get, user1.get.id.get)).friendship
